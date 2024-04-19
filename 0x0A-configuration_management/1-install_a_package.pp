57e4f4665ac8605c201e689d3654463f7f999cf8
Mwan3kii <agathawanja303@gmail.com>
2024-04-19 21:39:52 +0300

    attempting to fix checker 3

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,4 @@
 # using Puppet install flask from pip3.
-package{'puppet-lint':
-ensure   => '2.5.0',
-provider => 'gem'
+exec { 'puppet-lint':
+  command => '/usr/bin/apt-get -y install puppet-lint -v 2.5.0',
 }
