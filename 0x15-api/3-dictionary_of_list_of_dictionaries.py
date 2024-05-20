0496ae1053e2f4fd9d26731c64c72d4f35459da6
Mwan3kii <agathawanja303@gmail.com>
2024-05-20 22:29:30 +0300

    ro do em

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -2,25 +2,18 @@
 """Python script to export data in the JSON format"""
 import json
 import requests
-base_url = "https://jsonplaceholder.typicode.com"
-user_resp = requests.get(f"{base_url}/users")
-users = user_resp.json()
-todo_resp = requests.get(f"{base_url}/todos")
-todos = todo_resp.json()
-task_dict = {}
-for user in users:
-    user_id = user['id']
-    user_name = user['username']
-    user_todos = [todo for todo in todos if todo['userId'] == user_id]
-    task_list = []
-    for todo in user_todos:
-        task_info = {
-            "username": user_name,
-            "task": todo['title'],
-            "completed": todo['completed']
-        }
-        task_list.append(task_info)
-    task_dict[user_id] = task_list
-    with open("todo_all_employees.json", "w") as json_file:
-        json.dump(task_dict, json_file)
-print("Data has been exported to todo_all_employees.json")
+
+
+
+if __name__ == "__main__":
+    base_url = "https://jsonplaceholder.typicode.com/"
+    users = requests.get(base_url + "users").json()
+    with open("todo_all_employees.json", "w") as jsonfile:
+        json.dump({
+            u.get("id"): [{
+                "task": td.get("title"),
+                "completed": td.get("completed"),
+                "username": u.get("username")
+            } for td in requests.get(base_url + "todos",
+                                    params={"userId": u.get("id")}).json()]
+            for u in users}, jsonfile)
