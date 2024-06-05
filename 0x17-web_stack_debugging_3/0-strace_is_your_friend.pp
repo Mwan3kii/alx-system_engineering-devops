189709452b92b09be159081951b2d9207ba77769
Mwan3kii <agathawanja303@gmail.com>
2024-06-05 23:52:12 +0300

    apache error

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,11 +1,5 @@
 # Using strace, Puppet manifest to fix Apache 500 error
-file { '/etc/apache2/sites-available/000-default.conf':
-  ensure => file,
-  content => "ServerName localhost\n",
-}
-
-service { 'apache2':
-  ensure => running,
-  enable => true,
-  require => File['/etc/apache2/sites-available/000-default.conf'],
+exec { 'fix-wordpress':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
 }
