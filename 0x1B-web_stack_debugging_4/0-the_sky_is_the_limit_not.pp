565b17df60106efb86f6f9fff0b900f454f3351c
Mwan3kii <agathawanja303@gmail.com>
2024-06-13 21:28:31 +0300

    update puppet file

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,4 +1,11 @@
-exec { 'fix--for-nginx':
-  command => "sed -i 's/worker_processes 4;/worker_processes 7;/g' /etc/nginx/nginx.conf; sudo service nginx restart",
-  path    => ['/bin', '/usr/bin', '/usr/sbin']
+# File that fix stack to get 0
+file { 'replace last line':
+    ensure  => present,
+    path    => '/etc/default/nginx',
+    content => 'ULIMIT="-n 4096"',
+}
+
+service { 'nginx':
+    ensure    => running,
+    subscribe => File['/etc/default/nginx']
 }
